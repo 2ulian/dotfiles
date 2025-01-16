5983b0d9bcb88c56cdd5a70674cab7325f7a3627
fellwin <rayconstantyjulian@gmail.com>
2025-01-16 21:32:54 +0100

    Dotfiles for hyprland

---
+++ dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#installation de pywal et police
+yay -S --noconfirm --needed python-pywal16 python-pywalfox pywal-spicetify ttf-press-start-2p
+
+sudo pacman -S --needed ttf-hack-nerd
+
+#installation de hyprland:
+yay -S --noconfirm --needed hyprland-git hyprpicker-git waypaper hyprshade ags-hyprpanel-git
+
+sudo pacman -S --needed swww wofi kitty
